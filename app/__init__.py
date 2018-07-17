84efa225c60bc4340480c1efd0d90f02b5e018f9
Nick Persico <nickpersico@gmail.com>
2018-07-17 01:12:03 -0400

    requirements file

--- app/__init__.py
+++ app/__init__.py
@@ -13,19 +13,3 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models, errors
-
-
-# Log any errors to a file when not in debug mode
-if not app.debug:
-
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/sample_lead_importer.log', maxBytes=10240,
-                                       backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('Sample Lead Importer')
